08452c4156a8663169c7cb40967ab138173be571
Tibor_Pityo <tibor.pityo@interien.com>
2024-04-01 22:33:53 +0200

    user repo interface

---
+++ service-repository/Repositories/UserRepo/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using service_data.Models.EntityModels;
+
+namespace service_repository.Repositories.UserRepo
+{
+    public interface IUserRepository
+    {
+        Task<User> Create();
+        Task<User> Update();
+        Task<User> Delete();
+        Task<User> DeleteAsync();
+    }
+}
